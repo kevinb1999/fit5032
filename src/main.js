7abdad0d3e16029205b58a386ccdfe94c60e26af
kevinb1999 <kevinbalapitiya6@gmail.com>
2024-08-05 15:02:20 +1000

    activity4

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import './assets/main.css'
-import './style.css'
+// import './assets/main.css'
+// import './style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
