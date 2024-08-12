7844251cfd7901d1b819dac5d8885f3abe7419ad
kevinb1999 <kevinbalapitiya6@gmail.com>
2024-08-12 15:10:17 +1000

    activity 3

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -8,7 +8,15 @@
           <div class="row mb-3">
             <div class="col-md-6">
               <label for="username" class="form-label">Username</label>
-              <input type="text" class="form-control" id="username" required v-model="formData.username" />
+              <input
+                type="text"
+                class="form-control"
+                id="username"
+                @blur="() => validateName(true)"
+                @input="() => validateName(false)"
+                v-model="formData.username"
+              />
+              <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
             </div>
             <div class="col-md-6">
               <label for="password" class="form-label">Password</label>
@@ -16,11 +24,11 @@
                 type="password"
                 class="form-control"
                 id="password"
-                minlength="4"
-                maxlength="10"
-                required
+                @blur="() => validatePassword(true)"
+                @input="() => validatePassword(false)"
                 v-model="formData.password"
               />
+              <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
             </div>
           </div>
           <div class="row mb-3">
@@ -37,11 +45,18 @@
             </div>
             <div class="col-md-6">
               <label for="gender" class="form-label">Gender</label>
-              <select class="form-select form-control" id="gender" required v-model="formData.gender">
+              <select
+                class="form-select form-control"
+                id="gender"
+                @blur="() => validateGender(true)"
+                @input="() => validateGender(false)"
+                v-model="formData.gender"
+              >
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 <option value="other">Other</option>
               </select>
+              <div v-if="errors.gender" class="text-danger">{{ errors.gender }}</div>
             </div>
           </div>
           <div class="mb-3">
@@ -50,11 +65,11 @@
               class="form-control"
               id="reason"
               rows="3"
+              @blur="() => validateReason(true)"
+              @input="() => validateReason(false)"
               v-model="formData.reason"
-              minlength="2"
-              maxlength="240"
-              required
             ></textarea>
+            <div v-if="errors.reason" class="text-danger">{{ errors.reason }}</div>
           </div>
           <div class="text-center">
             <button type="submit" class="btn btn-primary me-2">Submit</button>
@@ -65,8 +80,13 @@
     </div>
   </div>
   <!-- Submited Cards -->
-  <div class="row mt-5 offset-1 offset-sm-2 offset-md-3 offset-lg-4 offset-xl-1" v-if="submittedCards.length">
-    <div class="d-flex flex-wrap justify-content-start col-11 col-sm-10 col-md-9 col-lg-8 col-xl-12">
+  <div
+    class="row mt-5 offset-1 offset-sm-2 offset-md-3 offset-lg-4 offset-xl-1"
+    v-if="submittedCards.length"
+  >
+    <div
+      class="d-flex flex-wrap justify-content-start col-11 col-sm-10 col-md-9 col-lg-8 col-xl-12"
+    >
       <div
         v-for="(card, index) in submittedCards"
         :key="index"
@@ -102,13 +122,88 @@ const formData = ref({
 const submittedCards = ref([])
 
 const submitForm = () => {
-  submittedCards.value.push({
-    ...formData.value
-  })
+  validateName(true)
+  validatePassword(true)
+  validateGender(true)
+  validateReason(true)
+  if (
+    !errors.value.username &&
+    !errors.value.password &&
+    !errors.value.gender &&
+    !errors.value.reason
+  ) {
+    submittedCards.value.push({
+      ...formData.value
+    })
+  }
 }
 
 const clearForm = () => {
   submittedCards.value = []
+}
+
+const errors = ref({
+  username: null,
+  password: null,
+  resident: null,
+  gender: null,
+  reason: null
+})
+
+const validateName = (blur) => {
+  if (formData.value.username.length < 3) {
+    if (blur) errors.value.username = 'Name must be at least 3 characters'
+  } else {
+    errors.value.username = null
+  }
+}
+
+const validatePassword = (blur) => {
+  const password = formData.value.password
+  const minLength = 8
+  const hasUppercase = /[A-Z]/.test(password)
+  const hasLowercase = /[a-z]/.test(password)
+  const hasNumber = /\d/.test(password)
+  const hasSpecialChar = /[!@#\$%\^\&*\)\(+=._-]/.test(password)
+
+  if (password.length < minLength) {
+    if (blur) errors.value.password = `Password must be at least ${minLength} characters long.`
+  } else if (!hasUppercase) {
+    if (blur) errors.value.password = 'Password must contain at least one uppercase letter.'
+  } else if (!hasLowercase) {
+    if (blur) errors.value.password = 'Password must contain at least one lowercase letter.'
+  } else if (!hasNumber) {
+    if (blur) errors.value.password = 'Password must contain at least one number.'
+  } else if (!hasSpecialChar) {
+    if (blur) errors.value.password = 'Password must contain at least one special character.'
+  } else {
+    errors.value.password = null
+  }
+}
+
+const validateGender = (blur) => {
+  if (!formData.value.gender) {
+    if (blur)
+      errors.value.gender = 'You must select a gender (Choose other if you prefer not to say)'
+  } else {
+    errors.value.gender = null
+  }
+}
+
+const validateReason = (blur) => {
+  const reason = formData.value.reason
+  const minLength = 2
+  const maxLength = 150
+
+  if (reason.length < minLength) {
+    if (blur)
+      errors.value.reason = `Reason for joining must be at least ${minLength} characters long. If you do not have a reason type N/A.`
+  } else if (reason.length > maxLength) {
+    if (blur)
+      errors.value.reason = `Reason for joining must be no more than ${maxLength} characters long.`
+  } else {
+    errors.value.reason = null
+  }
 }
 </script>
 
