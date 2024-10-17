2be842f6d08348a4e19550011ab3604f0136958e
Kevin <kevinbalapitiya6@gmail.com>
2024-10-18 10:25:45 +1100

    Week 11 https fix

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -70,7 +70,7 @@
       //Get the current weather icon using the API link
       iconUrl() {
         return this.weatherData
-          ? `http://api.openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
+          ? `https://api.openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
           : null;
       },
     },
@@ -90,7 +90,7 @@
             navigator.geolocation.getCurrentPosition(async (position) => {
                 const { latitude, longitude } = position.coords;
                 //API link to obtain the current weather based on the current location browser identified 
-                const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+                const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
                 //await means wait for the fetchWeatherData method to complete before proceeding
                 await this.fetchWeatherData(url);
             });
@@ -107,7 +107,7 @@
         },
         async searchByCity() {
             if (this.city.trim() !== "") {
-                const url = `http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}`;
+                const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${apikey}`;
                 await this.fetchWeatherData(url);
             } else {
                 alert("Please enter a city name");
